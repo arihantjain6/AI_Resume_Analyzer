004218084ec72eb0220aa8256f1f79f26790e3e2
arihantjain6 <arihantjainog@gmail.com>
2026-05-31 12:28:44 +0530

    chore: setup project for production deployment

--- Backend/app.js
+++ Backend/app.js
@@ -4,13 +4,18 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./src/routes/auth.routes.js";
 import interviewRoutes from "./src/routes/interview.routes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import cors from "cors";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   }),
 );
@@ -19,5 +24,11 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/interview", interviewRoutes);
+
+app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+});
 
 export default app;
